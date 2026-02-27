037bf8c037cc71e266c32ca16ada6cc091251f31
shreetej <wadwaleshreetej@gmail.com>
2026-02-27 11:20:16 +0000

    day1 added

---
+++ day1/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3) // we are using node js not browser
+let name = "shreetej"
+let age = 46
+let isLoggedIn = false
+
+// number = 2 to power 53 
+// biginit
+// string ""
+// boolean 
+//null = standalone value 
+// undefined =
+// symbol = unique 
+
+// object
+
+console.log(typeof undefined) // undefined 
+console.log(typeof null) // object
---
+++ day1/variable.js
@@ -0,0 +1,16 @@
+const accountId = 1234532
+let accountEmail = "abe@google.com"
+var accountPassword = "12998303"
+accountCity = "Jaipur"
+
+accountEmail ="aed@gmail"
+accountPassword ="12365656"
+accountCity ="mumbai"
+let accountState ;
+
+/* 
+prefer not to use var
+bcz of issue in block scope and functional scope */
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountState])
